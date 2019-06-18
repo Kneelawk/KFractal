e07aade982dfc84be9e0972729d9e8526a86940c
Kneelawk <jedidiah.pommert@gmail.com>
2019-06-17 17:56:08 -0700

    Add variable declaration attributes

---
+++ generatorapi/src/main/java/com/kneelawk/kfractal/generator/api/ir/attribute/Preallocated.java
@@ -0,0 +1,18 @@
+package com.kneelawk.kfractal.generator.api.ir.attribute;
+
+/**
+ * Preallocated - Variables declared with the preallocated attribute must be pointer type and cannot be reassigned. When
+ * a pointer is preallocated, it means that it starts as a valid memory handle to memory who's lifetime is the same as
+ * this variable. When a preallocated pointer goes out of scope, its memory is released.
+ */
+public class Preallocated implements IAttribute {
+	static final Preallocated INSTANCE = new Preallocated();
+
+	private Preallocated() {
+	}
+
+	@Override
+	public void accept(IAttributeVisitor visitor) {
+		visitor.visitPreallocated();
+	}
+}
