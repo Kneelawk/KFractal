c3882c9225dfdf6fc17857961d7a93e109563329
Kneelawk <jedidiah.pommert@gmail.com>
2019-06-28 15:42:35 -0700

    Add variable type checks

--- generatorvalidation/src/main/java/com/kneelawk/kfractal/generator/validation/ProgramValidator.java
+++ generatorvalidation/src/main/java/com/kneelawk/kfractal/generator/validation/ProgramValidator.java
@@ -1,21 +1,22 @@
 package com.kneelawk.kfractal.generator.validation;
 
 import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Maps;
-import com.kneelawk.kfractal.generator.api.ir.FractalIRException;
-import com.kneelawk.kfractal.generator.api.ir.FunctionDefinition;
-import com.kneelawk.kfractal.generator.api.ir.Program;
-import com.kneelawk.kfractal.generator.api.ir.VariableDeclaration;
+import com.kneelawk.kfractal.generator.api.ir.*;
+import com.kneelawk.kfractal.generator.api.ir.attribute.IAttribute;
 import com.kneelawk.kfractal.generator.api.ir.instruction.IInstruction;
 import org.apache.commons.lang3.tuple.ImmutablePair;
 import org.apache.commons.lang3.tuple.Pair;
 
 import java.util.Collection;
 import java.util.Map;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 public class ProgramValidator {
 	public static void checkValidity(Program program) throws FractalIRException {
+		checkVariables(program.getGlobalVariables().values());
 		Map<String, ValueInfo> globalVariables = ImmutableMap.copyOf(program.getGlobalVariables().values().stream()
 				.map(v -> new ImmutablePair<>(v.getName(),
 						new ValueInfo.Builder(true, v.getType(), v.getAttributes()).build()))
@@ -25,15 +26,18 @@ public class ProgramValidator {
 			// setup local variable scope
 			Map<String, ValueInfo> localScope = Maps.newHashMap();
 			localScope.putAll(globalVariables);
+			checkArguments(function.getContextVariableList());
 			checkVariableNameConflicts(localScope, function.getContextVariableList());
 			localScope.putAll(function.getContextVariableList().stream()
 					.map(v -> new ImmutablePair<>(v.getName(),
 							new ValueInfo.Builder(true, v.getType(), v.getAttributes()).build()))
 					.collect(Collectors.toMap(Pair::getKey, Pair::getValue)));
+			checkArguments(function.getArgumentList());
 			checkVariableNameConflicts(localScope, function.getArgumentList());
 			localScope.putAll(function.getArgumentList().stream().map(v -> new ImmutablePair<>(v.getName(),
 					new ValueInfo.Builder(true, v.getType(), v.getAttributes()).build()))
 					.collect(Collectors.toMap(Pair::getKey, Pair::getValue)));
+			checkVariables(function.getLocalVariables().values());
 			checkVariableNameConflicts(localScope, function.getLocalVariables().values());
 			localScope.putAll(function.getLocalVariables().values().stream().map(v -> new ImmutablePair<>(v.getName(),
 					new ValueInfo.Builder(true, v.getType(), v.getAttributes()).build()))
@@ -44,6 +48,57 @@ public class ProgramValidator {
 
 			for (IInstruction instruction : function.getBody()) {
 				instruction.accept(visitor);
+			}
+		}
+	}
+
+	private static void checkArguments(Collection<VariableDeclaration> arguments) throws FractalIRValidationException {
+		for (VariableDeclaration v : arguments) {
+			ValueType type = v.getType();
+			Set<IAttribute> attributes = v.getAttributes();
+
+			// check variable types
+			checkVariableType(type);
+
+			// check variable attributes
+			if (attributes.contains(IAttribute.PREALLOCATED)) {
+				throw new FractalIRValidationException("Illegal argument attribute: PREALLOCATED");
+			}
+		}
+	}
+
+	private static void checkVariables(Collection<VariableDeclaration> variables)
+			throws FractalIRValidationException {
+		for (VariableDeclaration v : variables) {
+			ValueType type = v.getType();
+			Set<IAttribute> attributes = v.getAttributes();
+
+			// check variable types
+			checkVariableType(type);
+
+			// check variable attributes
+			if (!ValueTypes.isPointer(type)) {
+				if (attributes.contains(IAttribute.PREALLOCATED)) {
+					throw new FractalIRValidationException("Illegal variable attribute: PREALLOCATED");
+				}
+			}
+
+			if (attributes.containsAll(ImmutableSet.of(IAttribute.CONSTANT, IAttribute.PREALLOCATED))) {
+				throw new FractalIRValidationException("Redundant variable attributes: CONSTANT and PREALLOCATED");
+			}
+		}
+	}
+
+	private static void checkVariableType(ValueType type) throws FractalIRValidationException {
+		if (ValueTypes.isVoid(type)) {
+			throw new FractalIRValidationException("Illegal variable type: VOID");
+		} else if (ValueTypes.isFunction(type)) {
+			if (ValueTypes.toFunction(type).isNullFunction()) {
+				throw new FractalIRValidationException("Illegal variable type: NULL_FUNCTION");
+			}
+		} else if (ValueTypes.isPointer(type)) {
+			if (ValueTypes.toPointer(type).isNullPointer()) {
+				throw new FractalIRValidationException("Illegal variable type: NULL_POINTER");
 			}
 		}
 	}
