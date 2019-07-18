0b643180b4e34619ee92d759ff4261cbb359e91e
Kneelawk <jedidiah.pommert@gmail.com>
2019-07-17 20:38:03 -0700

    Remove useless constant attribute

--- generatorapi/src/main/java/com/kneelawk/kfractal/generator/api/ir/attribute/IAttribute.java
+++ generatorapi/src/main/java/com/kneelawk/kfractal/generator/api/ir/attribute/IAttribute.java
@@ -1,7 +1,6 @@
 package com.kneelawk.kfractal.generator.api.ir.attribute;
 
 public interface IAttribute {
-    IAttribute CONSTANT = Constant.INSTANCE;
     IAttribute PREALLOCATED = Preallocated.INSTANCE;
 
     void accept(IAttributeVisitor visitor);
