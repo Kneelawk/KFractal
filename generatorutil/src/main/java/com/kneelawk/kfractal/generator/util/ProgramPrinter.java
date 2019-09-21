1063222c43c24a561ee4d44fb440c3546e919630
Kneelawk <jedidiah.pommert@gmail.com>
2019-09-21 04:28:45 -0700

    Fix ProgramPrinter

--- generatorutil/src/main/java/com/kneelawk/kfractal/generator/util/ProgramPrinter.java
+++ generatorutil/src/main/java/com/kneelawk/kfractal/generator/util/ProgramPrinter.java
@@ -1,11 +1,13 @@
 package com.kneelawk.kfractal.generator.util;
 
 import com.kneelawk.kfractal.generator.api.FractalException;
-import com.kneelawk.kfractal.generator.api.ir.FunctionDefinition;
-import com.kneelawk.kfractal.generator.api.ir.Program;
-import com.kneelawk.kfractal.generator.api.ir.GlobalDeclaration;
+import com.kneelawk.kfractal.generator.api.ir.*;
 import com.kneelawk.kfractal.generator.api.ir.attribute.IGlobalAttribute;
+import com.kneelawk.kfractal.generator.api.ir.phi.Phi;
+import com.kneelawk.kfractal.generator.api.ir.phi.PhiBranch;
 import com.kneelawk.kfractal.util.StringUtils;
+
+import java.util.List;
 
 public class ProgramPrinter {
     public static String printProgram(Program program) {
@@ -20,7 +22,7 @@ public class ProgramPrinter {
             builder.append(System.lineSeparator());
             StringUtils.indent(builder, 1);
             builder.append(i).append("=");
-            printVariableDeclaration(builder, variable);
+            printGlobalDeclaration(builder, variable);
             first = false;
         }
         if (first) {
@@ -54,99 +56,82 @@ public class ProgramPrinter {
 
     private static void printFunctionDefinition(StringBuilder builder, FunctionDefinition function) {
         builder.append("FunctionDefinition(").append(System.lineSeparator());
+
+        // print return type
         StringUtils.indent(builder, 2);
         builder.append(function.getReturnType().toString()).append(",").append(System.lineSeparator());
-        StringUtils.indent(builder, 2);
-        builder.append("[");
-        boolean first = true;
-        for (int i = 0; i < function.getContextVariables().size(); i++) {
-            GlobalDeclaration variable = function.getContextVariables().get(i);
-            if (!first)
-                builder.append(",");
-            builder.append(System.lineSeparator());
-            StringUtils.indent(builder, 3);
-            builder.append(i).append("=");
-            printVariableDeclaration(builder, variable);
-            first = false;
-        }
-        if (first) {
-            builder.append("  ");
-        } else {
-            builder.append(System.lineSeparator());
-            StringUtils.indent(builder, 2);
-        }
-        builder.append("],").append(System.lineSeparator());
-        StringUtils.indent(builder, 2);
-        builder.append("[");
-        first = true;
-        for (int i = 0; i < function.getArguments().size(); i++) {
-            GlobalDeclaration argument = function.getArguments().get(i);
-            if (!first)
-                builder.append(",");
-            builder.append(System.lineSeparator());
-            StringUtils.indent(builder, 3);
-            builder.append(i).append("=");
-            printVariableDeclaration(builder, argument);
-            first = false;
-        }
-        if (first) {
-            builder.append("  ");
-        } else {
-            builder.append(System.lineSeparator());
-            StringUtils.indent(builder, 2);
-        }
-        builder.append("],").append(System.lineSeparator());
-        StringUtils.indent(builder, 2);
-        builder.append("[");
-        first = true;
-        for (int i = 0; i < function.getLocalVariables().size(); i++) {
-            GlobalDeclaration variable = function.getLocalVariables().get(i);
-            if (!first)
-                builder.append(",");
-            builder.append(System.lineSeparator());
-            StringUtils.indent(builder, 3);
-            builder.append(i).append("=");
-            printVariableDeclaration(builder, variable);
-            first = false;
-        }
-        if (first) {
-            builder.append("  ");
-        } else {
-            builder.append(System.lineSeparator());
-            StringUtils.indent(builder, 2);
-        }
-        builder.append("],").append(System.lineSeparator());
-        StringUtils.indent(builder, 2);
-        builder.append("[");
 
-        InstructionPrinter printer = new InstructionPrinter(builder, 3);
-
-        first = true;
-        for (IInstruction instruction : function.getBlocks()) {
-            if (!first)
+        // print context variables
+        StringUtils.indent(builder, 2);
+        builder.append("[");
+        List<ArgumentDeclaration> arguments = function.getContextVariables();
+        int size = arguments.size();
+        int index;
+        for (index = 0; index < function.getContextVariables().size(); index++) {
+            ArgumentDeclaration variable = arguments.get(index);
+            if (index > 0)
                 builder.append(",");
             builder.append(System.lineSeparator());
             StringUtils.indent(builder, 3);
-            try {
-                instruction.accept(printer);
-            } catch (FractalException e) {
-                e.printStackTrace();
-            }
-            first = false;
+            builder.append(index).append("=");
+            printArgumentDeclaration(builder, variable);
         }
-        if (first) {
+        if (size < 1) {
+            builder.append("  ");
+        } else {
+            builder.append(System.lineSeparator());
+            StringUtils.indent(builder, 2);
+        }
+        builder.append("],").append(System.lineSeparator());
+
+        // print arguments
+        StringUtils.indent(builder, 2);
+        builder.append("[");
+        arguments = function.getArguments();
+        size = arguments.size();
+        for (index = 0; index < size; index++) {
+            ArgumentDeclaration argument = arguments.get(index);
+            if (index > 0)
+                builder.append(",");
+            builder.append(System.lineSeparator());
+            StringUtils.indent(builder, 3);
+            builder.append(index).append("=");
+            printArgumentDeclaration(builder, argument);
+        }
+        if (size < 1) {
+            builder.append("  ");
+        } else {
+            builder.append(System.lineSeparator());
+            StringUtils.indent(builder, 2);
+        }
+        builder.append("],").append(System.lineSeparator());
+
+        // print basic blocks
+        builder.append("[");
+        List<BasicBlock> blocks = function.getBlocks();
+        size = blocks.size();
+        for (index = 0; index < size; index++) {
+            BasicBlock block = blocks.get(index);
+            if (index > 0)
+                builder.append(",");
+            builder.append(System.lineSeparator());
+            StringUtils.indent(builder, 3);
+            printBasicBlock(builder, block);
+        }
+        if (size < 1) {
             builder.append("  ");
         } else {
             builder.append(System.lineSeparator());
             StringUtils.indent(builder, 2);
         }
         builder.append("]").append(System.lineSeparator());
+
         StringUtils.indent(builder, 1);
         builder.append(")");
     }
 
-    private static void printVariableDeclaration(StringBuilder builder, GlobalDeclaration declaration) {
-        builder.append("VariableDeclaration(");
+    private static void printGlobalDeclaration(StringBuilder builder, GlobalDeclaration declaration) {
+        builder.append("GlobalDeclaration(");
         builder.append(declaration.getType().toString());
         if (declaration.getAttributes().isEmpty()) {
             builder.append(")");
@@ -166,5 +151,103 @@ public class ProgramPrinter {
             }
             builder.append(" ])");
         }
+    }
+
+    private static void printArgumentDeclaration(StringBuilder builder, ArgumentDeclaration declaration) {
+        builder.append("ArgumentDeclaration(");
+        builder.append(declaration.getType().toString());
+        builder.append(")");
+    }
+
+    private static void printBasicBlock(StringBuilder builder, BasicBlock basicBlock) {
+        builder.append("BasicBlock(").append(System.lineSeparator());
+
+        // print phis
+        StringUtils.indent(builder, 4);
+        builder.append("[").append(System.lineSeparator());
+        int index;
+        int size = basicBlock.getPhis().size();
+        for (index = 0; index < size; index++) {
+            Phi phi = basicBlock.getPhis().get(index);
+            if (index > 0)
+                builder.append(",");
+            builder.append(System.lineSeparator());
+            StringUtils.indent(builder, 5);
+            builder.append(index).append("=");
+            printPhi(builder, phi);
+        }
+        if (size > 0) {
+            builder.append("  ");
+        } else {
+            builder.append(System.lineSeparator());
+            StringUtils.indent(builder, 4);
+        }
+        builder.append("],").append(System.lineSeparator());
+
+        // print instructions
+        ValuePrinter printer = new ValuePrinter(builder);
+
+        StringUtils.indent(builder, 4);
+        builder.append("[");
+        List<IValue> body = basicBlock.getBody();
+        size = body.size();
+        for (index = 0; index < size; index++) {
+            IValue value = body.get(index);
+            if (index > 0)
+                builder.append(",");
+            builder.append(System.lineSeparator());
+            StringUtils.indent(builder, 5);
+            builder.append(index).append("=");
+            try {
+                value.accept(printer);
+            } catch (FractalException e) {
+                e.printStackTrace();
+            }
+        }
+        if (size < 1) {
+            builder.append("  ");
+        } else {
+            builder.append(System.lineSeparator());
+            StringUtils.indent(builder, 4);
+        }
+        builder.append("]").append(System.lineSeparator());
+
+        StringUtils.indent(builder, 3);
+        builder.append(")");
+    }
+
+    private static void printPhi(StringBuilder builder, Phi phi) {
+        builder.append("Phi([").append(System.lineSeparator());
+
+        // print branches
+        boolean first = true;
+        for (PhiBranch branch : phi.getBranches()) {
+            if (!first)
+                builder.append(",");
+            builder.append(System.lineSeparator());
+            StringUtils.indent(builder, 6);
+            printPhiBranch(builder, branch);
+            first = false;
+        }
+        if (first) {
+            builder.append("  ");
+        } else {
+            builder.append(System.lineSeparator());
+            StringUtils.indent(builder, 5);
+        }
+        builder.append("])");
+    }
+
+    private static void printPhiBranch(StringBuilder builder, PhiBranch phiBranch) {
+        builder.append("PhiBranch(");
+
+        // print value
+        try {
+            phiBranch.getValue().accept(new PhiInputPrinter(builder));
+        } catch (FractalException e) {
+            e.printStackTrace();
+        }
+
+        builder.append(", ").append(phiBranch.getPreviousBlockIndex()).append(")");
     }
 }
