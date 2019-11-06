65a141dc09a80420921923d68ed837204df59610
Kneelawk <jedidiah.pommert@gmail.com>
2019-11-05 18:39:25 -0800

    Modify Program object to handle string-named globals

--- generatorapi/src/main/java/com/kneelawk/kfractal/generator/api/ir/Program.java
+++ generatorapi/src/main/java/com/kneelawk/kfractal/generator/api/ir/Program.java
@@ -1,14 +1,15 @@
 package com.kneelawk.kfractal.generator.api.ir;
 
 import com.google.common.base.Suppliers;
-import com.google.common.collect.ImmutableList;
-import com.google.common.collect.Lists;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Maps;
 import com.kneelawk.kfractal.util.KFractalToStringStyle;
 import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.tuple.Pair;
 
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.List;
+import java.util.Map;
 import java.util.function.Supplier;
 import java.util.stream.Collectors;
 
@@ -16,20 +17,20 @@ import java.util.stream.Collectors;
  * Created by Kneelawk on 5/25/19.
  */
 public class Program {
-    private List<GlobalDeclaration> globalVariables;
-    private List<FunctionDefinition> functions;
+    private Map<String, GlobalDeclaration> globalVariables;
+    private Map<String, FunctionDefinition> functions;
 
-    private Program(List<GlobalDeclaration> globalVariables,
-                    List<FunctionDefinition> functions) {
+    private Program(Map<String, GlobalDeclaration> globalVariables,
+                    Map<String, FunctionDefinition> functions) {
         this.globalVariables = globalVariables;
         this.functions = functions;
     }
 
-    public List<GlobalDeclaration> getGlobalVariables() {
+    public Map<String, GlobalDeclaration> getGlobalVariables() {
         return globalVariables;
     }
 
-    public List<FunctionDefinition> getFunctions() {
+    public Map<String, FunctionDefinition> getFunctions() {
         return functions;
     }
 
@@ -42,131 +43,141 @@ public class Program {
     }
 
     public static Program create(
-            List<GlobalDeclaration> globalVariables,
-            List<FunctionDefinition> functions) {
-        return new Program(ImmutableList.copyOf(globalVariables), ImmutableList.copyOf(functions));
+            Map<String, GlobalDeclaration> globalVariables,
+            Map<String, FunctionDefinition> functions) {
+        return new Program(ImmutableMap.copyOf(globalVariables), ImmutableMap.copyOf(functions));
     }
 
     public static class Builder {
-        private List<Supplier<GlobalDeclaration>> globalVariables = Lists.newArrayList();
-        private List<Supplier<FunctionDefinition>> functions = Lists.newArrayList();
+        private Map<String, Supplier<GlobalDeclaration>> globalVariables = Maps.newHashMap();
+        private Map<String, Supplier<FunctionDefinition>> functions = Maps.newHashMap();
 
         public Builder() {
         }
 
-        public Builder(Collection<Supplier<GlobalDeclaration>> globalVariables,
-                       Collection<Supplier<FunctionDefinition>> functions) {
-            this.globalVariables.addAll(globalVariables);
-            this.functions.addAll(functions);
+        public Builder(Map<String, Supplier<GlobalDeclaration>> globalVariables,
+                       Map<String, Supplier<FunctionDefinition>> functions) {
+            this.globalVariables.putAll(globalVariables);
+            this.functions.putAll(functions);
         }
 
         public Program build() {
-            return new Program(globalVariables.stream().map(Supplier::get).collect(ImmutableList.toImmutableList()),
-                    functions.stream().map(Supplier::get).collect(ImmutableList.toImmutableList()));
+            return new Program(
+                    globalVariables.entrySet().stream().map(e -> Pair.of(e.getKey(), e.getValue().get()))
+                            .collect(ImmutableMap.toImmutableMap(Pair::getKey, Pair::getValue)),
+                    functions.entrySet().stream().map(e -> Pair.of(e.getKey(), e.getValue().get()))
+                            .collect(ImmutableMap.toImmutableMap(Pair::getKey, Pair::getValue)));
         }
 
-        public List<Supplier<GlobalDeclaration>> getGlobalVariables() {
+        public Map<String, Supplier<GlobalDeclaration>> getGlobalVariables() {
             return globalVariables;
         }
 
-        public int getNextGlobalVariableIndex() {
-            return globalVariables.size();
-        }
-
         public Builder setGlobalVariableSuppliers(
-                Collection<Supplier<GlobalDeclaration>> globalVariables) {
+                Collection<Map.Entry<String, Supplier<GlobalDeclaration>>> globalVariables) {
             this.globalVariables.clear();
-            this.globalVariables.addAll(globalVariables);
+            this.globalVariables.putAll(ImmutableMap.copyOf(globalVariables));
             return this;
         }
 
-        public Builder setGlobalVariables(Collection<GlobalDeclaration> globalVariables) {
+        public Builder setGlobalVariables(Collection<Map.Entry<String, GlobalDeclaration>> globalVariables) {
             this.globalVariables.clear();
-            this.globalVariables
-                    .addAll(globalVariables.stream().map(Suppliers::ofInstance).collect(Collectors.toList()));
+            this.globalVariables.putAll(globalVariables.stream()
+                    .map(e -> Pair.of(e.getKey(), Suppliers.ofInstance(e.getValue())))
+                    .collect(Collectors.toMap(Pair::getKey, Pair::getValue)));
             return this;
         }
 
-        public int addGlobalVariable(Supplier<GlobalDeclaration> declaration) {
-            globalVariables.add(declaration);
-            return globalVariables.size() - 1;
+        public Builder addGlobalVariable(String name, Supplier<GlobalDeclaration> declaration) {
+            globalVariables.put(name, declaration);
+            return this;
         }
 
-        public int addGlobalVariable(GlobalDeclaration declaration) {
-            globalVariables.add(Suppliers.ofInstance(declaration));
-            return globalVariables.size() - 1;
+        public Builder addGlobalVariable(String name, GlobalDeclaration declaration) {
+            globalVariables.put(name, Suppliers.ofInstance(declaration));
+            return this;
         }
 
         @SafeVarargs
-        public final Builder addGlobalVariables(Supplier<GlobalDeclaration>... declarations) {
-            globalVariables.addAll(Arrays.asList(declarations));
+        public final Builder addGlobalVariableSuppliers(
+                Map.Entry<String, Supplier<GlobalDeclaration>>... declarations) {
+            globalVariables.putAll(Arrays.stream(declarations)
+                    .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue)));
             return this;
         }
 
-        public final Builder addGlobalVariables(GlobalDeclaration... declarations) {
-            globalVariables.addAll(Arrays.stream(declarations).map(Suppliers::ofInstance).collect(Collectors.toList()));
+        @SafeVarargs
+        public final Builder addGlobalVariables(Map.Entry<String, GlobalDeclaration>... declarations) {
+            globalVariables.putAll(Arrays.stream(declarations)
+                    .map(p -> Pair.of(p.getKey(), Suppliers.ofInstance(p.getValue())))
+                    .collect(Collectors.toMap(Pair::getKey, Pair::getValue)));
             return this;
         }
 
-        public Builder addGlobalVariableSuppliers(Collection<Supplier<GlobalDeclaration>> declarations) {
-            globalVariables.addAll(declarations);
+        public Builder addGlobalVariableSuppliers(
+                Collection<Map.Entry<String, Supplier<GlobalDeclaration>>> declarations) {
+            globalVariables.putAll(ImmutableMap.copyOf(declarations));
             return this;
         }
 
-        public Builder addGlobalVariables(Collection<GlobalDeclaration> declarations) {
-            globalVariables.addAll(declarations.stream().map(Suppliers::ofInstance).collect(Collectors.toList()));
+        public Builder addGlobalVariables(Collection<Map.Entry<String, GlobalDeclaration>> declarations) {
+            globalVariables
+                    .putAll(declarations.stream().map(e -> Pair.of(e.getKey(), Suppliers.ofInstance(e.getValue())))
+                            .collect(Collectors.toMap(Pair::getKey, Pair::getValue)));
             return this;
         }
 
-        public List<Supplier<FunctionDefinition>> getFunctions() {
+        public Map<String, Supplier<FunctionDefinition>> getFunctions() {
             return functions;
         }
 
-        public int getNextFunctionIndex() {
-            return functions.size();
-        }
-
         public Builder setFunctionSuppliers(
-                Collection<Supplier<FunctionDefinition>> functions) {
+                Collection<Map.Entry<String, Supplier<FunctionDefinition>>> functions) {
             this.functions.clear();
-            this.functions.addAll(functions);
+            this.functions.putAll(ImmutableMap.copyOf(functions));
             return this;
         }
 
-        public Builder setFunctions(Collection<FunctionDefinition> functions) {
+        public Builder setFunctions(Collection<Map.Entry<String, FunctionDefinition>> functions) {
             this.functions.clear();
-            this.functions.addAll(functions.stream().map(Suppliers::ofInstance).collect(Collectors.toList()));
+            this.functions.putAll(functions.stream().map(e -> Pair.of(e.getKey(), Suppliers.ofInstance(e.getValue())))
+                    .collect(Collectors.toMap(Pair::getKey, Pair::getValue)));
             return this;
         }
 
-        public int addFunction(Supplier<FunctionDefinition> definition) {
-            functions.add(definition);
-            return functions.size() - 1;
+        public Builder addFunction(String name, Supplier<FunctionDefinition> definition) {
+            functions.put(name, definition);
+            return this;
         }
 
-        public int addFunction(FunctionDefinition definition) {
-            functions.add(Suppliers.ofInstance(definition));
-            return functions.size() - 1;
+        public Builder addFunction(String name, FunctionDefinition definition) {
+            functions.put(name, Suppliers.ofInstance(definition));
+            return this;
         }
 
         @SafeVarargs
-        public final Builder addFunctions(Supplier<FunctionDefinition>... definitions) {
-            functions.addAll(Arrays.asList(definitions));
+        public final Builder addFunctionSuppliers(Map.Entry<String, Supplier<FunctionDefinition>>... definitions) {
+            functions.putAll(Arrays.stream(definitions)
+                    .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue)));
             return this;
         }
 
-        public Builder addFunctions(FunctionDefinition... definitions) {
-            functions.addAll(Arrays.stream(definitions).map(Suppliers::ofInstance).collect(Collectors.toList()));
+        @SafeVarargs
+        public final Builder addFunctions(Map.Entry<String, FunctionDefinition>... definitions) {
+            functions
+                    .putAll(Arrays.stream(definitions).map(e -> Pair.of(e.getKey(), Suppliers.ofInstance(e.getValue())))
+                            .collect(Collectors.toMap(Pair::getKey, Pair::getValue)));
             return this;
         }
 
-        public Builder addFunctionSuppliers(Collection<Supplier<FunctionDefinition>> definitions) {
-            functions.addAll(definitions);
+        public Builder addFunctionSuppliers(Collection<Map.Entry<String, Supplier<FunctionDefinition>>> definitions) {
+            functions.putAll(ImmutableMap.copyOf(definitions));
             return this;
         }
 
-        public Builder addFunctions(Collection<FunctionDefinition> definitions) {
-            functions.addAll(definitions.stream().map(Suppliers::ofInstance).collect(Collectors.toList()));
+        public Builder addFunctions(Collection<Map.Entry<String, FunctionDefinition>> definitions) {
+            functions.putAll(definitions.stream().map(e -> Pair.of(e.getKey(), Suppliers.ofInstance(e.getValue())))
+                    .collect(Collectors.toMap(Pair::getKey, Pair::getValue)));
             return this;
         }
     }
